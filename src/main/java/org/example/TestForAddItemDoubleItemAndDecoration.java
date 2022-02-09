5acec1fb00f9f5c39ea00426f78597b7ec771d42
Angela <lik.bobrova@yandex.ru>
2022-02-09 10:11:26 +0300

    Lesson 3

--- src/main/java/org/example/TestForAddItemDoubleItemAndDecoration.java
+++ src/main/java/org/example/TestForAddItemDoubleItemAndDecoration.java
@@ -2,10 +2,13 @@ package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class TestForAddItemDoubleItemAndDecoration {
 
@@ -20,15 +23,29 @@ public class TestForAddItemDoubleItemAndDecoration {
         options.addArguments("disable-popup-blocking");
         Thread t = null;
         WebDriver driver = new ChromeDriver(options);
+
         driver.get("https://happy-sushi.ru/");
 
+        WebElement webElement = driver.findElement(By.xpath(".//form[@id='ms_form_147']"));
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].scrollIntoView();", webElement);
 
+        t.sleep(10000);
+        new WebDriverWait(driver, 5);
         driver.findElement(By.xpath(".//form[@id='ms_form_147']/button")).click();
 
         t.sleep(10000);
-        WebDriverWait webDriverWait2 = new WebDriverWait(driver, 5);
+        new WebDriverWait(driver, 5);
         driver.findElement(By.xpath(".//div[@id='msMiniCart']/div[@class='not_empty text-center']/a")).click();
         driver.findElement(By.xpath(".//div[@class='product-counter product-counter--big']/button[@class='product-counter--plus product-counter-btn']")).click();
+
+
+        WebElement element = driver.findElement(By.xpath(".//div[@class='basket-btn']/a"));
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].scrollIntoView();", element);
+
+        t.sleep(10000);
+        new WebDriverWait(driver, 5);
         driver.findElement(By.xpath(".//div[@class='basket-btn']/a")).click();
         //driver.close();
     }
--- src/main/java/org/example/TestForAddItemsInBasket.java
+++ src/main/java/org/example/TestForAddItemsInBasket.java
@@ -2,6 +2,7 @@ package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -24,13 +25,18 @@ public class TestForAddItemsInBasket {
         driver.get("https://happy-sushi.ru/");
 
         driver.findElement(By.xpath(".//div[@id='mixit-block']/div[@class='mixitBlock-menu']/button[@data-btn='cat17']")).click();
+
+        WebElement webElement = driver.findElement(By.xpath(".//form[@id='ms_form_137']"));
+        ((JavascriptExecutor) driver).executeScript(
+                "arguments[0].scrollIntoView();", webElement);
         t.sleep(10000);
-        WebDriverWait webDriverWait = new WebDriverWait(driver, 10);
+        new WebDriverWait(driver, 5);
 
         driver.findElement(By.xpath(".//form[@id='ms_form_137']/a")).click();
         driver.findElement(By.xpath(".//form[@id='product-form']/div[@class='product-page__content']/div[@class='row']/div[@class='col-lg-7 col-xl-6']/div[@class='product-set']/div[@class='product-set__footer']/div[@class='total-amount']/button")).click();
+
         t.sleep(10000);
-        WebDriverWait webDriverWait1 = new WebDriverWait(driver, 5);
+        new WebDriverWait(driver, 5);
         driver.findElement(By.xpath(".//div[@id='msMiniCart']/div[@class='not_empty text-center']/a")).click();
 
         //driver.close();
